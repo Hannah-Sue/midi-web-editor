ba63b99869b50dae68ecbc505a282b1400ad642b
suhaina <2694256463@qq.com>
2026-05-20 17:54:48 +0800

    Agent功能开发前存档

---
+++ backend/src/main/java/suhaina/midieditor/midieditorbackend/MidiEditorBackendApplication.java
@@ -0,0 +1,13 @@
+package suhaina.midieditor.midieditorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidiEditorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidiEditorBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/suhaina/midieditor/midieditorbackend/MidiEditorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package suhaina.midieditor.midieditorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidiEditorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
